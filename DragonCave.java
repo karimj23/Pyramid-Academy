f654215a50ec12f79058550cf3bcd0599065abcc
Karim Jooma <58650166+karimj23@users.noreply.github.com>
2021-10-18 01:05:29 -0400

    Add files via upload

---
+++ DragonCave.java
@@ -0,0 +1,13 @@
+package DragonCave;
+import java.util.Scanner;
+public class DragonCave {
+    public static void main(String[] args) {
+        System.out.println("You are in a land full of dragons. In front of you,\n" +
+                "\n" +
+                "you see two caves. In one cave, the dragon is friendly\n" +
+                "\n" +
+                "and will share his treasure with you. The other dragon\n" +
+                "\n" +
+                "is greedy and hungry and will eat you on sight.\n" +
+                "\n" +
+                "Which cave will you go into? (1 or 2)");
