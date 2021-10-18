ad186f6ee0ee12738190ab51204f2f4c0278d7f3
Karim Jooma <58650166+karimj23@users.noreply.github.com>
2021-10-18 01:43:35 -0400

    Add files via upload

--- DragonCave.java
+++ DragonCave.java
@@ -1,13 +1,28 @@
-package DragonCave;
+package src;
 import java.util.Scanner;
 public class DragonCave {
     public static void main(String[] args) {
         System.out.println("You are in a land full of dragons. In front of you,\n" +
-                "\n" +
                 "you see two caves. In one cave, the dragon is friendly\n" +
-                "\n" +
                 "and will share his treasure with you. The other dragon\n" +
-                "\n" +
                 "is greedy and hungry and will eat you on sight.\n" +
-                "\n" +
                 "Which cave will you go into? (1 or 2)");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if(num ==1){
+            System.out.println("You approach the cave...\n" +
+                    "It is dark and spooky...\n" +
+                    "A large dragon jumps out in front of you! He opens his jaws and...\n" +
+                    "Gobbles you down in one bite!");
+        }else if(num ==2){
+            System.out.println("You approach the cave...\n" +
+                    "You find gold and diamonds...\n");
+        }else{
+            System.out.println("Please try again!");
+        }
+    }}
+/// Questions for this assignment
+//Are dragons really real for real?
+//Answer No!
+//How does one get input from the player?
+//Scanner scan = new Scanner(System.in)
